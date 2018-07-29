b21489a0873d47e9106ea9cfae0f937902c7bf19
周能 <724761135@qq.com>
2018-07-29 21:31:12 +0800

    7.29

--- JS/js2/js/check.js
+++ JS/js2/js/check.js
@@ -2,72 +2,74 @@
 var data = JSON.parse(localStorage.getItem('key'));
 
 // 返回按钮返回到发牌页面
-function backtrack(){
+function backtrack() {
     location.href = "../html/Player.html";
 }
 
 // 关闭按钮返回到主页面
-function off(){
-     // 点击关闭弹出提示窗口
-    if(confirm("是否要退出游戏返回到主页面")){
-        location.href="../html/start.html";//点击确定返回到主页面
-    }else{
-        return false;//点击取消停留在当前页面
+function off() {
+    // 点击关闭弹出提示窗口
+    if (confirm("是否要退出游戏返回到主页面")) {
+        location.href = "../html/start.html"; //点击确定返回到主页面
+    } else {
+        return false; //点击取消停留在当前页面
     }
 }
 
-var verso=document.getElementsByClassName("picture")[0];
-var front=document.getElementsByClassName("circle-box")[0];
-var player=document.getElementsByClassName("describe")[0];
+// 获取背面图片dom节点
+var verso = document.getElementsByClassName("picture")[0];
+// 获取正面图片角色盒子dom节点
+var front = document.getElementsByClassName("circle-box")[0];
+// 获取角色描述dom节点
+var player = document.getElementsByClassName("describe")[0];
+// 获取玩家数量dom节点
+var count = document.getElementsByClassName("figure")[0];
+// 获取button按钮dom节点
+var check = document.getElementById("button");
 
+var arr = []; //声明一个空数组
+var arr = data; //把获取到本地的数据传到数组里
 
-// var arr=[];//声明一个空数组
-// var arr=data;//把获取到本地的数据传到数组里
-// var m=-1;
+var i = 0;
+var m = 2;
 
-// function button(){
-//     console.log(arr)
-//     arr[m];
-//     m++;
-//     console.log(arr[m]);
-
-// //     var a=0;
-    
-// //     if(a<=m){
-// //         document.getElementById("aaa").innerHTML = m + 1;
-// //         console.log(document.getElementById("aaa").innerHTML);
-//     }
-
-
-// // if(m>arr.length-1){
-// //     location.href = "../html/Player.html";
-// // }
-//     // for(var i=0; i<arr.length; i++){
-//     //     m=" " + i + " " + arr[i];
-//     //     console.log(m);
-//     // }
-
-// }
-
-
-
-
-var verso=document.getElementsByClassName("picture")[0];
-var front=document.getElementsByClassName("circle-box")[0];
-
-var a=0;
-var b
-function button(){
-    z= [];
-    if(a){
-        verso.style.display ="none";
-        front.style.display ="inline-block";
+function button() {
+    // 
+    if (i % 1 == 0) {
+        //隐藏反面图片
+        verso.style.display = "none";
+        //显示正面图片和玩家描述盒子
+        front.style.display = "inline-block";
+        // 修改button的内容
+        check.innerText = ("隐藏并传递" + m + "号");
+        // 获取数组内容
+        player.innerText = (arr[i]);
+        i = i + 0.5;
+        m = m + 1;
+        console.log(arr)
+        console.log(player)
+        console.log(check)
+        console.log(i)
+    } else
+    if (i != 0) {
+        var bb = m - 1;
+        // 显示反面图片
+        verso.style.display = "inline-block";
+        // 隐藏正面图片和玩家描述盒子
+        front.style.display = "none";
+        // 修改button内容
+        check.innerText = ("查看" + bb + "号身份");
+        // 修改玩家数量值
+        count.innerText = m - 1;
+        i = i + 0.5;
+        console.log(i)
     }
-     if (b=front){
-        verso.style.display ="inline-block";
-        front.style.display ="none";
+    if (m > arr.length + 1) {
+        // 大于数组长度修改button按钮值
+        check.innerText = ("法官查看");
     }
-    verso.style.display ="none";
-    front.style.display ="inline-block";
+    if (i >= arr.length) {
+        // 大于等于数组长度进入下一个页面
+        location.href = "../html/Player.html";
+    }
 }
-
