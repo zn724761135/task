0c411deb24638b4b037e42bd6cb44eef9dac0396
周能 <724761135@qq.com>
2018-08-01 23:17:50 +0800

    7.1

--- JS/js2/js/libretto.js
+++ JS/js2/js/libretto.js
@@ -17,3 +17,53 @@ function off() {
 // 获取保存到本地的玩家属性的对象数组的数据
 var data = JSON.parse(localStorage.getItem("store"));
 console.log(data)
+
+
+window.onload = function () {
+
+    var b = document.getElementsByTagName("h4");
+    var c = document.getElementsByTagName("ul");
+
+    for (let i = 0; i < b.length; i++) {
+        b[i].index = i;
+        b[i].onclick = function () {
+            if (this.className == "") {
+                c[this.index].style.display = "none";
+                this.className = "block";
+            } else {
+                c[this.index].style.display = "block";
+                this.className = "";
+            }
+
+
+        }
+
+    }
+
+}
+
+// window.onload = function (){
+//     var oUl = document.getElementById('list');
+//     var aH2 = document.getElementsByTagName('h4');
+//     var aUl = this.document.getElementsByTagName('ul');
+
+//     for(var i=0;i<aH2.length;i++){
+//         aH2[i].index=i;
+//         aH2[i].onclick = function(){
+//             for(var i=0;i<aH2.length;i++){
+//                 if(aH2[i] !=this){
+//                     aUl[i].style.display='none';
+//                     aH2[i].className='';
+//                 }
+//             }
+//             if(this.className==''){
+//                 aUl[this.index].style.display='none';
+//                 this.className='active';
+//             }else{
+//                 aUl[this.index].style.display='block';
+//                 this.className='';
+//             }
+//         };
+//     }
+
+// };
