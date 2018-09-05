c646f18868525c355f10c3d49db1248e9bede108
周能 <724761135@qq.com>
2018-09-06 00:30:02 +0800

    9.6

--- JS/js6-10/js/app.js
+++ JS/js6-10/js/app.js
@@ -15,23 +15,34 @@
 
 
 // ui路由
-
-angular.module('myApp', ["ui.router","oc.lazyLoad"])
+angular.module('myApp', ["ui.router", "oc.lazyLoad"]) //加载ui路由模块和懒加载模块
     .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/login');
+        $urlRouterProvider.otherwise('/login'); //默认加载页面
         $stateProvider
-            .state('login', {
-                url: '/login',
-                views: {
+            .state('login', { //路由跳转
+                url: '/login', //定义路由的地址
+                views: { //视窗，加载路由的html模块
                     '': {
                         templateUrl: 'html/login.html',
                     }
                 },
-                resolve: { 
-                    loadMyCtrl: (function ($ocLazyLoad) {
-                        return $ocLazyLoad.load(["css/login.css"]);
+                resolve: { //懒加载，加载html模块对应的css和js文件
+                    myload: (function ($ocLazyLoad) {
+                        return $ocLazyLoad.load(["css/login.css", "js/login.js"]);
                     })
                 }
             })
-
+            .state('home', {
+                url: '/home',
+                views: {
+                    '': {
+                        templateUrl: 'html/home.html',
+                    }
+                },
+                resolve:{
+                    myload:(function($ocLazyLoad){
+                        return $ocLazyLoad.load(["css/home.css"]);
+                    })
+                }
+            })
     })
